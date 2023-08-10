9913365e5ebc6511ab2d235a0de96be9337f3ee0
ghamlet <dmtr317744@gmail.com>
2023-08-10 23:32:28 +0300

    fee

--- subpoc.py
+++ subpoc.py
@@ -1,13 +1,45 @@
 import subprocess
 import re
 
+let = False
+point_counter = 0
+point = 0
+
+
+
+pattern = re.compile("\d{1,3}\.{1}\d{1,3}\.{1}\d{1,3}\.{1}\d{1,3}")
+
+
 def run_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
-    output = output.decode()
-    #print(output)# error.decode()))
-    for line in output:
-        if re.search("IPv4 Address", line):
-            print(line)
+    global cat
+    cat = output.decode()
+    return cat
 
-run_command("ipconfig")
+
+run_command("ipconfig")   
+
+
+coincidence = re.search(pattern, cat, flags=0)
+if coincidence:
+    print("ipaddr", coincidence[0])
+
+# for i in range(x,len):
+#     if (cat[i] == "." or cat[i] == " ") and let == False:
+        
+#         continue
+#     elif cat[i] == ":":
+#         addr = "".join([addr, ""])
+#         let = True
+
+#     elif let == True and point_counter == False:
+#         addr = "".join([addr, cat[i]])
+#         point+=1
+#         if point ==3:
+#             point_counter = True
+
+#     else:
+#         addr = "".join([addr, cat[i]])
+
+
